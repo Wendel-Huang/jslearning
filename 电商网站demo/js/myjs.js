9d6c182d1fc5b815cd9fd74521acf5e2fa433bee
hwy <470637232@qq.com>
2018-08-10 13:35:49 +0800

    0810

---
+++ 电商网站demo/js/myjs.js
@@ -0,0 +1,11 @@
+// window.onload=function(){
+// 	for (var i = 1; i <11; i++) {
+// 		var classname= 'bg'+i;
+// 		var oA=document.getElementsByClassName(classname)[0];
+// 		oA.index=i;
+// 		oA.onmouseover=function(){
+// 			var theleft=(1-this.index)*66;
+// 			this.style.backgroundPosition=(0,0);
+// 		}
+// 	}
+// }
